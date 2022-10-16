c6b4dde20b4ef6916d610955fe4116800d65b49c
Gevindu-Piyumal <gevindu.personal@gmail.com>
2022-10-16 17:57:30 +0530

    done...

--- Something/wwwroot/js/site.js
+++ Something/wwwroot/js/site.js
@@ -2,3 +2,24 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+//window.onload = function () {
+//        Particles.init
+//            ({
+//                selector: '.background',
+//                maxParticles: 30,
+//                speed:.4,
+//                connectParticles: true
+//            });
+//};
+
+
+
+//selector	string - Required: CSS selector of your canvas element
+//maxParticles	integer	100	Optional: Maximum amount of particles
+//sizeVariations	integer	3	Optional: Amount of size variations
+//speed	integer	0.5	Optional: Movement speed of the particles
+//color	string or string[]	#000000	Optional: Color(s) of the particles and connecting lines
+//minDistance	integer	120	Optional: Distance in px for connecting lines
+//connectParticles	boolean	false	Optional: true / false if connecting lines should be drawn
+//responsive	array	null	Optional: Array of objects containing breakpoints and options
